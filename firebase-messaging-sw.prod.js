7a0a7484f0927c2d3cf4adec9cbcf4902eae884d
Gross <igross@estee.com>
2022-01-20 21:17:37 -0500

    Updates

--- firebase-messaging-sw.prod.js
+++ firebase-messaging-sw.prod.js
@@ -4,17 +4,6 @@ importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-firestore.js');
 
-const broadcast = new BroadcastChannel('channel-123');
-
-let id = '';
-
-broadcast.onmessage = ({ data }) => {
-    const { action, payload } = data;
-    if (action === "SET_ID") {
-        id = payload;
-    }
-}
-
 const firebaseApp = firebase.initializeApp({
     apiKey: "AIzaSyBTYpqPswzYpqIlf-sfdLoDJroew3hPBCM",
     authDomain: "shulapp.firebaseapp.com",
@@ -44,26 +33,19 @@ messaging.onBackgroundMessage((payload) => {
     const notificationOptions = {
         body,
         data: payload,
-        icon: '/firebase-logo.png',
         actions: [
             { action: 'Yes', title: 'Yes', minyan },
-            { action: 'Maybe', title: 'Maybe', minyan },
-            { action: 'No', title: 'No', minyan },
+            { action: 'No', title: 'No', minyan }
         ]
     };
+
     closeAllNotifications()
-    self.registration.showNotification(title,
-        notificationOptions);
+    self.registration.showNotification(title, notificationOptions);
 });
 
 
 self.addEventListener('notificationclick', (e) => {
-    const { action, notification: { data: { data: payload } } } = e
-    console.log("in middleman event", { action, payload, e })
+    const { action, notification: { data: { data: { minyan, date } } } } = e
 
-    self.clients.openWindow(`/ShulApp?action=${action}&minyan=${payload?.minyan}`).then((data) => {
-        console.log('data', { data })
-        broadcast.postMessage({ action, payload })
-        closeAllNotifications();
-    });
+    self.clients.openWindow(`/ShulApp?action=${action}&minyan=${minyan}&date=${date}&env=dev`).then(closeAllNotifications);
 });
